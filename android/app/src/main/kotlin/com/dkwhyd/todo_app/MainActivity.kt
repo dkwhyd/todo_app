599a5078b01d75c6de0f6e340b8d8baa252ed601
dkwhyd <dikiwhyd99@gmail.com>
2024-03-29 14:58:12 +0700

    initializing

---
+++ android/app/src/main/kotlin/com/dkwhyd/todo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dkwhyd.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/todo_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoDetail extends StatefulWidget {
+  const TodoDetail({super.key});
+
+  @override
+  State<TodoDetail> createState() => _TodoDetailState();
+}
+
+class _TodoDetailState extends State<TodoDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
