ee0806db27742025e8b63b06d07156a8f333f9e5
dkwhyd <dikiwhyd99@gmail.com>
2024-03-29 15:00:40 +0700

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App (bloc)',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
